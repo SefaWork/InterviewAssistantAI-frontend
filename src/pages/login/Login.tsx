9827384a8b2756f72cfb49f737508a45af65c455
SefaWork <mehmetsefatoksoy@gmail.com>
2026-01-13 20:18:30 +0300

    Add wip login form and style

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,23 @@
+// wip
 import './Login.css'
 
 function Login() {
     return (
-        <div className="login-page">
-
+        <div className='login-page'>
+            <div className='login-form-container'>
+                <h1>Login</h1>
+                <form className='login-form'>
+                    <div className='form-input'>
+                        <label htmlFor='email'>E-Mail Address:</label>
+                        <input type='text' id='email' name='email' autoComplete='email'></input>
+                    </div>
+                    <div className='form-input'>
+                        <label htmlFor='email'>Password:</label>
+                        <input type='password' id='password' name='password' autoComplete='password'></input>
+                    </div>
+                    <input type="submit"></input>
+                </form>
+            </div>
         </div>
     )
 }
