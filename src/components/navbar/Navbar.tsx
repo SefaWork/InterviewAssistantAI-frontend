3402dfbe158249d794c29f3250fee9095a6d0379
SefaWork <mehmetsefatoksoy@gmail.com>
2026-01-17 03:43:04 +0300

    Refactor Header and Navbar to add login link and better style

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,16 @@
 import './Navbar.css'
 import DarkModeToggle from './DarkModeToggle'
+import { Link } from 'react-router-dom'
+import { useAuth } from '../../hooks/useAuth'
 
 function Navbar() {
+    const authContext = useAuth();
+
     return (
-        <section className="navbar">
+        <section className='navbar'>
+            <div className='navbar-link-container'>
+                {authContext.authTokens? (<Link to='/account'>Account</Link>) : (<Link to='/login'>Login</Link>) }
+            </div>
             <DarkModeToggle />
         </section>
     )
