3967979b2dd7f6c3e706e73e34c20202431b5f82
SefaWork <mehmetsefatoksoy@gmail.com>
2026-02-28 22:04:10 +0300

    Add wip websocket for interview process.

--- src/pages/interview/InterviewPage.tsx
+++ src/pages/interview/InterviewPage.tsx
@@ -1,11 +1,47 @@
 import Webcam from 'react-webcam'
 import './InterviewPage.css'
+import { useEffect, useRef } from 'react';
+
+// 1 second / FPS
+const SEND_INTERVAL = 1_000 / 5
 
 function InterviewPage() {
+
+    const webcamRef = useRef<Webcam>(null);
+    const ws = useRef<WebSocket | null>(null);
+
+    // Create a new WebSocket. (WIP.)
+    useEffect(() => {
+        ws.current = new WebSocket("ws://localhost:8000/ws/webcam/")
+        return () => {
+            ws.current?.close()
+        }
+    }, [])
+
+    const sendFrame = () => {
+        const imgSrc = webcamRef.current?.getScreenshot();
+
+        // Null guard. (typescript is annoying sometimes)
+        if(!imgSrc) {
+            console.warn("Webcam component is not fully initialized. (Missing user permissions?)")
+            return;
+        }
+
+        ws.current?.send(JSON.stringify(imgSrc))
+    }
+
+    // Create interval to send frames.
+    useEffect(() => {
+        const intervalID = setInterval(sendFrame, SEND_INTERVAL)
+        return () => {
+            clearInterval(intervalID)
+        }
+    }, [])
+
     return (
         <div className="interview-main-div">
             <div className='webcam-section'>
-                <Webcam />
+                <Webcam ref={webcamRef} mirrored={true} screenshotFormat='image/jpeg'/>
             </div>
             <div className='instruction-section'>
                 <h1>Question 1</h1>
