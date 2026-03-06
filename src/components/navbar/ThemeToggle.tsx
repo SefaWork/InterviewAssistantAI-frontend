ddbc796647c02c9fb8623a66e0f1714cba6c9d57
SefaWork <mehmetsefatoksoy@gmail.com>
2026-03-06 17:59:28 +0300

    Fix: state value updated inside effect, leading to lint error

--- src/components/navbar/ThemeToggle.tsx
+++ src/components/navbar/ThemeToggle.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext } from 'react'
 import ThemeContext from '../../context/ThemeContext'
 
 import LightModeIcon from '../../assets/brightness-high.svg'
@@ -16,25 +16,17 @@ import './ThemeToggle.css'
 function ThemeToggle() {
     // Local storage dark mode state.
     const context = useContext(ThemeContext);
-    if (!context) return;
-
-    const [imgSrc, setImgSrc] = useState<[string, string]>([LightModeIcon, LightModeHoverIcon]);
-
-    useEffect(() => {
-        if (context.darkMode) {
-            setImgSrc([DarkModeIcon, DarkModeHoverIcon])
-        } else {
-            setImgSrc([LightModeIcon, LightModeHoverIcon])
-        }
-    }, [context.darkMode])
+    if (!context) {
+        throw new Error("ThemeContext is required to render this component.")
+    }
 
     return (
         <div 
             className='theme-toggle-container'
             onClick={context.toggleDarkMode}
         >
-        <img src={imgSrc[0]} className='theme-toggle'></img>
-        <img src={imgSrc[1]} className='theme-toggle-hover'></img>
+        <img src={context.darkMode? DarkModeIcon : LightModeIcon} className='theme-toggle'></img>
+        <img src={context.darkMode? DarkModeHoverIcon : LightModeHoverIcon} className='theme-toggle-hover'></img>
         </div>
     )
 }
