2d045b95da2ed1fb1a73dd817c46a9f7bdfd88e3
SefaWork <mehmetsefatoksoy@gmail.com>
2026-03-17 10:57:36 +0300

    Modify and localize about page.

--- src/pages/home/About.tsx
+++ src/pages/home/About.tsx
@@ -1,10 +1,21 @@
+import { useTranslation } from 'react-i18next';
 import './About.css'
 
 function About() {
+    const {t} = useTranslation();
+
     return (
         <div className="about-page">
-            <h1>About Us</h1>
-            <p>InterviewHelper is a project that was developed to help those who are anxious about their non-verbal performance. Non-verbal communication plays an important role in physical interviews.</p>
+            <h1>{t("about_page.objective_title")}</h1>
+            <p>{t("about_page.objective")}</p>
+            <h1>{t("about_page.features_title")}</h1>
+            <ul>
+                <li>{t("about_page.features.1")}</li>
+                <li>{t("about_page.features.2")}</li>
+                <li>{t("about_page.features.3")}</li>
+                <li>{t("about_page.features.4")}</li>
+                <li>{t("about_page.features.5")}</li>
+            </ul>
         </div>
     )   
 }
