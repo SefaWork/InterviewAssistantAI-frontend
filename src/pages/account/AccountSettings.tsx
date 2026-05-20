8b3b4092c76ce115916cc563ac7161e640a4ad1e
SefaWork <mehmetsefatoksoy@gmail.com>
2026-05-20 15:22:36 +0300

    Add account settings template localization.

--- src/pages/account/AccountSettings.tsx
+++ src/pages/account/AccountSettings.tsx
@@ -3,12 +3,14 @@ import ChangeEmail from './ChangeEmail';
 import DeleteAccount from './DeleteAccount';
 import ChangePassword from './ChangePassword';
 import './AccountSettings.css'
+import { useTranslation } from 'react-i18next';
 
 export type AccountSettingsPageState = "main" | "change-password" | "change-email" | "delete-account";
 
 function AccountSettings() {
     // Set e-mail, set password, delete account.
     const [pageState, setPageState] = useState<AccountSettingsPageState>("main");
+    const {t} = useTranslation();
 
     useEffect(() => {
         console.log("Doing stuff...");
@@ -19,17 +21,17 @@ function AccountSettings() {
             <div className={`settings-window ${pageState}`}>
                 {pageState === "main" && (
                     <>
-                        <h1>Account Settings</h1>
-                        <button className='settings-button primary' onClick={() => setPageState('change-email')}>Change E-Mail</button>
-                        <button className='settings-button primary' onClick={() => setPageState('change-password')}>Change Password</button>
-                        <button className='settings-button danger' onClick={() => setPageState('delete-account')}>Delete Account</button>
+                        <h1>{t("account_settings_page.account_settings")}</h1>
+                        <button className='settings-button primary' onClick={() => setPageState('change-email')}>{t("account_settings_page.change_email")}</button>
+                        <button className='settings-button primary' onClick={() => setPageState('change-password')}>{t("account_settings_page.change_password")}</button>
+                        <button className='settings-button danger' onClick={() => setPageState('delete-account')}>{t("account_settings_page.delete_account")}</button>
                     </>
                 )}
                 {pageState === "change-email" && (<ChangeEmail />)}
                 {pageState === "change-password" && (<ChangePassword />)}
                 {pageState === "delete-account" && (<DeleteAccount />)}
                 {pageState !== "main" && (
-                    <button onClick={() => setPageState('main')}>Back</button>
+                    <a className='back-btn' onClick={() => setPageState('main')}>{t("back")}</a>
                 )}
             </div>
         </div>
--- src/pages/account/DeleteAccount.tsx
+++ src/pages/account/DeleteAccount.tsx
@@ -1,19 +1,22 @@
 import type { FormEvent } from "react";
 import FormPasswordField from "../../components/form/FormPasswordField";
+import { useTranslation } from "react-i18next";
 
 function DeleteAccount() {
-   // Child component to be rendered under the AccountSettings component.
+    // Child component to be rendered under the AccountSettings component.
+    const {t} = useTranslation();
+
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
     }
 
     return (
         <>
-            <h1>Delete Account</h1>
-            <p>Type in your password to the box below only if you are sure about deleting your account. <b>This action is irreversible!</b></p>
+            <h1>{t("account_settings_page.delete_account")}</h1>
+            <p>{t("account_settings_page.confirmation_text")} <b>{t("account_settings_page.deletion_warning")}</b></p>
             <form onSubmit={handleSubmit}>
                 <FormPasswordField id="password" />
-                <button type="submit" className="account-deletion-btn">Confirm Account Deletion</button>
+                <button type="submit" className="account-deletion-btn">{t("confirm")}</button>
             </form>
         </>
     )
