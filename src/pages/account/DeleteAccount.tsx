a99df72adcab69e37c188365f552213a25fe1ceb
SefaWork <mehmetsefatoksoy@gmail.com>
2026-05-24 22:56:00 +0300

    Add account deletion logic.

--- src/pages/account/DeleteAccount.tsx
+++ src/pages/account/DeleteAccount.tsx
@@ -1,13 +1,33 @@
-import type { FormEvent } from "react";
+import { useRef, type FormEvent } from "react";
 import FormPasswordField from "../../components/form/FormPasswordField";
 import { useTranslation } from "react-i18next";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+
+const DELETE_ACCOUNT_PATH = '/api/account/delete-account/'
 
 function DeleteAccount() {
     // Child component to be rendered under the AccountSettings component.
     const {t} = useTranslation();
+    const passwordRef = useRef<HTMLInputElement>(null);
+    const axiosServer = useAxiosPrivate();
+    const auth = useAuth();
+    const navigate = useNavigate();
 
-    const handleSubmit = (e: FormEvent) => {
+    const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
+
+        const givenPassword = passwordRef.current?.value
+        if (!givenPassword) return;
+
+        try {
+            await axiosServer.post(DELETE_ACCOUNT_PATH, {"password": givenPassword});
+            auth.setAccessToken(undefined);
+            navigate('/')
+        } catch(err) {
+            console.error(err);
+        }
     }
 
     return (
@@ -15,7 +35,7 @@ function DeleteAccount() {
             <h1>{t("account_settings_page.delete_account")}</h1>
             <p>{t("account_settings_page.confirmation_text")} <b>{t("account_settings_page.deletion_warning")}</b></p>
             <form onSubmit={handleSubmit}>
-                <FormPasswordField id="password" />
+                <FormPasswordField id="password" ref={passwordRef} />
                 <button type="submit" className="account-deletion-btn">{t("confirm")}</button>
             </form>
         </>
