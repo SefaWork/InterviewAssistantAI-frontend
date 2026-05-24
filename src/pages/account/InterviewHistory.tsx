a9d1def8990e78cbcb979b6020de62ea5241cf41
SefaWork <mehmetsefatoksoy@gmail.com>
2026-05-24 22:37:32 +0300

    Add interview record deletion.

--- src/pages/account/InterviewHistory.tsx
+++ src/pages/account/InterviewHistory.tsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
+
 import './InterviewHistory.css'
+import { Link } from 'react-router-dom';
 
 type HistorySummary = {
     id: string,
@@ -24,6 +26,7 @@ const formatDate = (date: Date) => {
 };
 
 const SESSION_LIST_PATH = '/api/account/interviews/'
+const SESSION_DELETE_PATH = '/api/account/delete-interview/'
 
 const deserializeResponse = (entry: {id: string, created_at: string, total_avg: number}): HistorySummary => {
     return {created_at: new Date(entry.created_at), id: entry.id, totalScore: entry.total_avg}
@@ -32,6 +35,7 @@ const deserializeResponse = (entry: {id: string, created_at: string, total_avg: 
 function InterviewHistory() {
     const [interviews, setInterviews] = useState<HistorySummary[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
+    const deletingStateRef = useRef<boolean>(false);
     const axiosServer = useAxiosPrivate();
 
     useEffect(() => {
@@ -48,13 +52,37 @@ function InterviewHistory() {
         return () => controller.abort();
     }, [axiosServer])
 
+    const handleDelete = async (e: React.MouseEvent<HTMLButtonElement>, id: string) => {
+        e.stopPropagation()
+
+        if (deletingStateRef.current) return;
+        deletingStateRef.current = true;
+
+        setInterviews((val) => {
+            return val.filter(oldVal => oldVal.id !== id)
+        })
+
+        try {
+            await axiosServer.post(SESSION_DELETE_PATH, {"session_id": id})
+        } catch(err) {
+            console.error(err)
+        } finally {
+            deletingStateRef.current = false;
+        }
+    }
+
     if (loading) return (<div className='loading'>Loading...</div>);
 
     return (
         <ol>
             {interviews.length <= 0 && <li>You haven't done any interviews!</li>}
             {interviews.map(interviewData => (
-                <li key={interviewData.id}>{formatDate(interviewData.created_at)} {interviewData.totalScore}%</li>
+                <li key={interviewData.id}>
+                    <Link to={`/history/${interviewData.id}`}>
+                        {formatDate(interviewData.created_at)} {interviewData.totalScore}%
+                    </Link>
+                    <button onClick={(e) => handleDelete(e, interviewData.id)}>Delete</button>
+                </li>
             ))}
         </ol>
     )
